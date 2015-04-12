f0fa5f484d6e6453856bf6d263b96c983cb3f089
Maxi Winkler <maxi.5a@outlook.com>
2015-04-12 20:11:37 +0200

    20:10h Sunday, 12/04 2015. Added Objective-C Files and modified storyboard

---
+++ Slide Menu/Slide Menu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slide Menu
+//
+//  Created by Maxi Winkler on 12.04.15.
+//  Copyright (c) 2015 Informaticamaxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
