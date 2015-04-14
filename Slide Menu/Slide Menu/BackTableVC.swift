e81906daa17fc3060c15876d98b8b670609c97b1
Maxi Winkler <maxi.5a@outlook.com>
2015-04-14 20:51:07 +0200

    Creates empty menu

---
+++ Slide Menu/Slide Menu/BackTableVC.swift
@@ -0,0 +1,19 @@
+//
+//  BackTableVC.swift
+//  Slide Menu
+//
+//  Created by Maxi Winkler on 14.04.15.
+//  Copyright (c) 2015 Informaticamaxi. All rights reserved.
+//
+
+import Foundation
+
+class BackTableVC: UITableViewController{
+    
+    var TableArray = [String]()
+    
+    override func viewDidLoad() {
+        TableArray = ["", "", ""]
+        
+    }
+}
